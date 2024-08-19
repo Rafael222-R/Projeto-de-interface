410eae4afea3bd478d71394c6636ed3a0907c32f
Rafael222-R <rafael_dias18@hotmail.com>
2024-08-18 21:57:13 -0300

    Initial commit

---
+++ Projeto de interface (1)sdsd/js (1).js
@@ -0,0 +1,20 @@
+// importação do bootstrap
+src = "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+
+//inicio
+// pop para a pagina de html
+alert ("Seja bem vindo"); // cubo branco é uma função ,cubo roxo para jogo usado para objetos, cubo azul são interfaces ex. cochete são matriz ou vetores 
+// variavel e while
+var contador = 0;
+while(contador < 5) {
+    alert("Sextouuuuuu");
+    contador = contador + 1;
+    // para recarregar o pop pela quantidade de vezes insirada no contador 
+}
+//input
+var nome;
+nome = prompt("Que Dia da semana?");
+alert("oi, " + nome);
+
+
---
+++ Projeto de interface (2)sdd/js.js
@@ -0,0 +1,10 @@
+// importação do bootstrap
+src = "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+
+
+var nome;
+nome = prompt("Qual o maior campeao da Champions league?");
+alert("O maior campeao é o Real Madrid!");
+
+
